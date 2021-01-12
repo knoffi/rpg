fa79266d9f3b6e48f3092c5d5209024c8398d0a0
knoffi <matthiastutor@outlook.de>
2021-01-12 16:51:48 +0100

    ADD #24 random food, drinks and base price

--- mainNavigator/getRandomStartTavern.ts
+++ mainNavigator/getRandomStartTavern.ts
@@ -1,31 +1,45 @@
-import { Adjective, association } from "../classes/Adjectives";
-import { getProductsLeftAndBannerData } from "../editNavigator/editNavigatorFunctions";
-import { getFittingRandom, getRandomArrayEntry } from "../helpingFunctions/getFittingRandom";
-import { misfitMode, stricterMisfitMode } from "../helpingFunctions/misfitModes";
-import { getMisfits } from "../helpingFunctions/misFitsHandlers";
-import { BasePrice } from "../scenes/menuScene/basePrice";
-import { Offer } from "../scenes/menuScene/menuEnums";
-import { adjectives, substantives } from "../scenes/nameScene/names/nouns";
-import { getTavernHistoryInitializer } from "./mainNavigatorFunctions";
+import { Adjective, association } from '../classes/Adjectives';
+import { getProductsLeftAndBannerData } from '../editNavigator/editNavigatorFunctions';
+import {
+    getFittingRandom,
+    getRandomArrayEntry,
+} from '../helpingFunctions/getFittingRandom';
+import {
+    misfitMode,
+    stricterMisfitMode,
+} from '../helpingFunctions/misfitModes';
+import { getMisfits } from '../helpingFunctions/misFitsHandlers';
+import { BasePrice } from '../scenes/menuScene/basePrice';
+import { drinkExamples } from '../scenes/menuScene/drinks/drinks';
+import { foodExamples } from '../scenes/menuScene/food/food';
+import { NothingLeftOffer, Offer } from '../scenes/menuScene/menuEnums';
+import {
+    getNewRandomDrinkOffer,
+    weServe,
+} from '../scenes/menuScene/menuFunctions';
+import { adjectives, substantives } from '../scenes/nameScene/names/nouns';
+import { getTavernHistoryInitializer } from './mainNavigatorFunctions';
 
 const CHANCE_FOR_SPECIAL_FIT = 0.2;
 const CHANCE_FOR_ORDINARY_FIT = 0.625;
-export const getRandomStartTavern=()=>{
+const NO_OFFER_PROBABILITY = 0.1;
+const MAX_OFFER = 4;
+export const getRandomStartTavern = () => {
     const tavernData = getTavernHistoryInitializer();
-   
-    const fits= getRandomFits();
-    const misfits=getMisfits(fits,misfitMode.stricter);
-    const basePrice=getRandomBasePrice()
-    const drinks= getRandomDrinkOffers(fits,misfits,basePrice);
-    const dishes = getRandomFoodOffers(fits,misfits,basePrice)
-    tavernData.name =getRandomName(fits,misfits);
+
+    const fits = getRandomFits();
+    const misfits = getMisfits(fits, misfitMode.stricter);
+    const basePrice = getRandomBasePrice();
+    const drinks = getRandomOffers(fits, misfits, weServe.drinks);
+    const dishes = getRandomOffers(fits, misfits, weServe.food);
+    tavernData.name = getRandomName(fits, misfits);
     tavernData.fitting = {
         fits: fits,
         misfits: misfits,
     };
     tavernData.drinks = drinks;
-    tavernData.dishes =      dishes;
-               const bannerData = getProductsLeftAndBannerData(
+    tavernData.dishes = dishes;
+    const bannerData = getProductsLeftAndBannerData(
         {
             fits: fits,
             misfits: misfits,
@@ -39,20 +53,65 @@ export const getRandomStartTavern=()=>{
     tavernData.foodBannerData = bannerData.foodBannerData!;
     tavernData.prices = basePrice;
     return tavernData;
-}
+};
 
-const getRandomFits=()=>{
+const getRandomFits = () => {
     const associationGroups = stricterMisfitMode.associationGroups;
-    const fitsWithEmptyFits=  Object.values(associationGroups).map(group=>{const chanceToAddFit = group ===stricterMisfitMode.associationGroups.special? CHANCE_FOR_SPECIAL_FIT : CHANCE_FOR_ORDINARY_FIT; return Math.random()<chanceToAddFit?getRandomArrayEntry(group):association.empty})
-    return fitsWithEmptyFits.filter(fit=>fit!==association.empty)
-}
-const getRandomBasePrice=()=>{
-    return {wealthy:50,rich:150,modest:17,poor:6,currency:"Dragon Coins"} as BasePrice
-}
+    const fitsWithEmptyFits = Object.values(associationGroups).map((group) => {
+        const chanceToAddFit =
+            group === stricterMisfitMode.associationGroups.special
+                ? CHANCE_FOR_SPECIAL_FIT
+                : CHANCE_FOR_ORDINARY_FIT;
+        return Math.random() < chanceToAddFit
+            ? getRandomArrayEntry(group)
+            : association.empty;
+    });
+    return fitsWithEmptyFits.filter((fit) => fit !== association.empty);
+};
+const getRandomBasePrice = () => {
+    const poorPrice = Math.floor(Math.random() * 5) + 2;
+    return {
+        wealthy: poorPrice * 9,
+        rich: poorPrice * 20,
+        modest: poorPrice * 3,
+        poor: poorPrice,
+        currency: 'Copper',
+    } as BasePrice;
+};
 
-const getRandomDrinkOffers=(fits:association[],misfits:association[],basePrice:BasePrice)=>{return [] as Offer[]}
-const getRandomFoodOffers=(fits:association[],misfits:association[],basePrice:BasePrice)=>{return [] as Offer[]}
-const getRandomName=(fits:association[],misfits:association[])=>{
+const getRandomOffers = (
+    fits: association[],
+    misfits: association[],
+    isAbout: weServe
+) => {
+    const exampleChapters =
+        isAbout === weServe.drinks ? drinkExamples : foodExamples;
+    return exampleChapters
+        .map((chapter) => {
+            const maxNumberOfDishes = Math.floor(
+                Math.random() * MAX_OFFER + (1 - NO_OFFER_PROBABILITY)
+            );
+            const offers: Offer[] = new Array(maxNumberOfDishes).fill(
+                NothingLeftOffer
+            );
+            offers.map((entry, index) => {
+                const previousOffers = offers.filter(
+                    (entry, checkIndex) => checkIndex < index
+                );
+                const newOffer = getNewRandomDrinkOffer(
+                    fits,
+                    misfits,
+                    chapter.category,
+                    previousOffers,
+                    isAbout
+                );
+                offers[index] = newOffer;
+            });
+            return offers;
+        })
+        .flat();
+};
+const getRandomName = (fits: association[], misfits: association[]) => {
     const adjective = getFittingRandom(
         adjectives,
         fits,
@@ -60,16 +119,12 @@ const getRandomName=(fits:association[],misfits:association[])=>{
         []
     ) as Adjective;
     const validSubstantiveChapters = substantives.filter(
-            (chapter) => !adjective.badWords.includes(chapter.category)
-        );
-        const validSubstantives= validSubstantiveChapters.flatMap(
-            (chapter) => chapter.substantives
-        );
-    const substantive =  getFittingRandom(
-            validSubstantives,
-            fits,
-            misfits,
-            []
-        );
+        (chapter) => !adjective.badWords.includes(chapter.category)
+    );
+    const validSubstantives = validSubstantiveChapters.flatMap(
+        (chapter) => chapter.substantives
+    );
+    const substantive = getFittingRandom(validSubstantives, fits, misfits, []);
 
-    return adjective.name + " " + substantive.name}
+    return adjective.name + ' ' + substantive.name;
+};
