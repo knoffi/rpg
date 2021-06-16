a286e7131ef3dd7375ef3c18a893fb73a424e6ac
knoffi <matthiastutor@outlook.de>
2021-06-16 17:28:59 +0200

    ADD #50 better red wine description

--- scenes/menuScene/drinks/wines/redWines.ts
+++ scenes/menuScene/drinks/wines/redWines.ts
@@ -12,27 +12,27 @@ export const redWines: DishIdea[] = [
             },
             firstSideDishes: [
                 {
-                    name: 'Red wine  -  dry, aged 4 years in oak barrels, smoky note, spicy finish',
+                    name: 'Red wine (dry)  -  aged 4 years in oak barrels, smoky note, spicy finish',
                     incomeRange: [a.rich],
                     price: 90,
                 },
                 {
-                    name: 'Red wine  -  dry, aged 3 years in oak barrels, smoky note, spicy finish',
+                    name: 'Red wine (dry)  -  aged 3 years in oak barrels, smoky note, spicy finish',
                     incomeRange: [a.rich],
                     price: 75,
                 },
                 {
-                    name: 'Red wine  -  dry, aged 6 years in oak barrels, smoky note, spicy finish',
+                    name: 'Red wine (dry)  -  aged 6 years in oak barrels, smoky note, spicy finish',
                     incomeRange: [a.rich],
                     price: 120,
                 },
                 {
-                    name: 'Red wine  -  dry, aged 5 years in oak barrels, smoky note, spicy finish',
+                    name: 'Red wine (dry)  -  aged 5 years in oak barrels, smoky note, spicy finish',
                     incomeRange: [a.rich],
                     price: 105,
                 },
                 {
-                    name: 'Red wine  -  dry, aged 2 years in oak barrels, smoky note, spicy finish',
+                    name: 'Red wine (dry)  -  aged 2 years in oak barrels, smoky note, spicy finish',
                     incomeRange: [a.rich, a.wealthy],
                     price: 60,
                 },
@@ -49,17 +49,17 @@ export const redWines: DishIdea[] = [
             },
             firstSideDishes: [
                 {
-                    name: 'Red wine  -  sweet, aged 1 year,  full-bodied, fruity note',
+                    name: 'Red wine (sweet)  -  aged 1 year,  full-bodied, fruity note',
                     incomeRange: [a.wealthy],
                     price: 28,
                 },
                 {
-                    name: 'Red wine  -  sweet, aged 2 years,  full-bodied, fruity note',
+                    name: 'Red wine (sweet)  -  aged 2 years,  full-bodied, fruity note',
                     incomeRange: [a.wealthy],
                     price: 39,
                 },
                 {
-                    name: 'Red wine  -  sweet, aged 3 years,  full-bodied, fruity note',
+                    name: 'Red wine (sweet)  -  aged 3 years,  full-bodied, fruity note',
                     incomeRange: [a.wealthy],
                     price: 50,
                 },
@@ -76,17 +76,17 @@ export const redWines: DishIdea[] = [
             },
             firstSideDishes: [
                 {
-                    name: 'Red wine  -  semi dry, aged 1 year,  light-bodied, cherry-note',
+                    name: 'Red wine (semi-dry)  -  aged 1 year,  light-bodied, cherry note',
                     incomeRange: [a.modest],
                     price: 18,
                 },
                 {
-                    name: 'Red wine  -  semi dry, aged 2 years,  light-bodied, cherry-note',
+                    name: 'Red wine (semi-dry)  -  aged 2 years,  light-bodied, cherry note',
                     incomeRange: [a.modest, a.wealthy],
                     price: 27,
                 },
                 {
-                    name: 'Red wine  -  semi dry, aged 3 years,  light-bodied, cherry-note',
+                    name: 'Red wine (semi-dry)  -  aged 3 years,  light-bodied, cherry note',
                     incomeRange: [a.wealthy],
                     price: 38,
                 },
@@ -103,7 +103,7 @@ export const redWines: DishIdea[] = [
             },
             firstSideDishes: [
                 {
-                    name: 'Red wine  -  sweet, rough fruityness',
+                    name: 'Red wine (sweet)  -  rough fruityness',
                     incomeRange: [a.modest],
                     price: 8,
                 },
@@ -120,17 +120,17 @@ export const redWines: DishIdea[] = [
             },
             firstSideDishes: [
                 {
-                    name: 'Red wine  -  dry, aged 1 year, fruity, light-bodied, notes of plum',
+                    name: 'Red wine (dry)  -  aged 1 year, fruity, light-bodied, notes of plum',
                     incomeRange: [a.modest, a.wealthy],
                     price: 30,
                 },
                 {
-                    name: 'Red wine  -  dry, aged 3 years, fruity, full-bodied, notes of plum',
+                    name: 'Red wine (dry)  -  aged 3 years, fruity, full-bodied, notes of plum',
                     incomeRange: [a.wealthy],
                     price: 60,
                 },
                 {
-                    name: 'Red wine  -  dry, aged 4 years, fruity, full-bodied, notes of plum',
+                    name: 'Red wine (dry)  -  aged 4 years, fruity, full-bodied, notes of plum',
                     incomeRange: [a.rich],
                     price: 90,
                 },
