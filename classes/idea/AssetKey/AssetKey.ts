500c90b2532c7d46a15464f99b19f5476ed2d189
knoffi <matthiastutor@outlook.de>
2021-07-19 15:09:20 +0200

    ADD #60 Asset keys for average customer

--- classes/idea/AssetKey/AssetKey.ts
+++ classes/idea/AssetKey/AssetKey.ts
@@ -1,10 +1,14 @@
 //NOTE: adding to AssetKey -> adding to getKeyBound
 export enum AssetKey {
     plotTwist = 'plotTwist',
-    servant = 'servant',
-    creatureTrophies = 'creature trophies',
-    barDeco = 'bar decoration',
-    tables = 'tables',
-    cutleryPlates = 'knife, fork, spoon & plate',
-    drinkHolder = 'mug, horn, cup & chalice',
+    INDIVIDUALS_servant = 'servant',
+    FURNITUE_creatureTrophies = 'creature trophies',
+    FURNITUE_barDeco = 'bar decoration',
+    FURNITUE_tables = 'tables',
+    FURNITUE_cutleryPlates = 'knife, fork, spoon & plate',
+    FURNITUE_drinkHolder = 'mug, horn, cup & chalice',
+    AVERAGE_CUSTOMER_drinking = 'drinking',
+    AVERAGE_CUSTOMER_smoking = 'smoking',
+    AVERAGE_CUSTOMER_eating = 'eating',
+    AVERAGE_CUSTOMER_behavior = 'actions, character, mood',
 }
