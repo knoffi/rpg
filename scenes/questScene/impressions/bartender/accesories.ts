37f4ddbb3d5b59940c115d37d870a9c96ce29fec
knoffi <matthiastutor@outlook.de>
2021-08-03 23:54:55 +0200

    ADD #66 bartender's lost limbs

--- scenes/questScene/impressions/bartender/accesories.ts
+++ scenes/questScene/impressions/bartender/accesories.ts
@@ -2,6 +2,7 @@ import { association } from '../../../../classes/association';
 import { AssetKey } from '../../../../classes/idea/AssetKey/AssetKey';
 import { ImpressionIdea } from '../../../../classes/idea/ImpressionIdea';
 import { Noticable } from '../../../../classes/idea/Noticable';
+import { defaultPowerFitConcepts } from '../../../../classes/idea/powerFitConcepts/powerFitConcepts';
 const a = association;
 export const bartenderAccesories: ImpressionIdea[] = [
     new ImpressionIdea(
@@ -46,5 +47,83 @@ export const bartenderAccesories: ImpressionIdea[] = [
         Noticable.bartender,
         undefined,
         true
+    ),
+    new ImpressionIdea(
+        {
+            name: 'Misses his ',
+            needsOne: [
+                a.barbarian,
+                a.soldier,
+                a.adventurer,
+                a.thief,
+                a.assasine,
+                a.knight,
+                a.prostitute,
+            ],
+            worksForAllCriminals: true,
+        },
+        [
+            {
+                name: 'left leg',
+                powerFits: [a.haven, a.soldier, a.thief],
+                needsOne: [a.haven, a.soldier],
+                worksForAllCriminals: true,
+            },
+            {
+                name: 'left hand',
+                worksForAllCriminals: true,
+                powerFits: [a.haven, a.soldier, a.thief, a.desert],
+                needsOne: [a.haven, a.soldier, a.desert],
+            },
+            {
+                name: 'left ear',
+                misfits: [a.haven],
+            },
+            {
+                name: 'left thumb',
+                misfits: [a.haven],
+            },
+            {
+                name: 'left arm',
+                misfits: [a.haven],
+            },
+            {
+                name: 'left eye',
+                powerFits: [a.haven, a.soldier, a.thief, a.barbarian],
+                needsOne: [a.haven, a.soldier, a.barbarian],
+            },
+            {
+                name: 'right leg',
+                powerFits: [a.haven, a.soldier, a.thief],
+                needsOne: [a.haven, a.soldier],
+            },
+            {
+                name: 'right hand',
+                worksForAllCriminals: true,
+                powerFits: [a.haven, a.soldier, a.thief, a.desert],
+                needsOne: [a.haven, a.soldier, a.desert],
+            },
+            {
+                name: 'right ear',
+                misfits: [a.haven],
+            },
+            {
+                name: 'right thumb',
+                misfits: [a.haven],
+            },
+            {
+                name: 'right eye',
+                powerFits: [a.haven, a.soldier, a.thief, a.barbarian],
+                needsOne: [a.haven, a.soldier, a.barbarian],
+            },
+            {
+                name: 'right arm',
+                misfits: [a.haven],
+            },
+        ],
+        Noticable.bartender,
+        undefined,
+        undefined,
+        defaultPowerFitConcepts.harmony
     ),
 ];
