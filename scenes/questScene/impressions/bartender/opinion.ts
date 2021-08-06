78af65fbe2a73292646feaf48b52361ec9a18ff7
knoffi <matthiastutor@outlook.de>
2021-08-06 10:56:36 +0200

    ADD #66 criminal tendency for bartender (dis)likes

--- scenes/questScene/impressions/bartender/opinion.ts
+++ scenes/questScene/impressions/bartender/opinion.ts
@@ -19,40 +19,48 @@ export const bartenderOpinions: ImpressionIdea[] = [
                 name: 'dwarves',
                 powerFits: [a.elf],
                 misfits: [a.human, a.dwarf, a.gnome],
+                worksForAllCriminals: true,
             },
             {
                 name: 'elves',
                 powerFits: [a.dwarf],
                 misfits: [a.human, a.elf, a.halfling],
+                worksForAllCriminals: true,
             },
             {
                 name: 'barbarians',
                 powerFits: [a.wizard, a.rich],
                 misfits: [a.barbarian, a.adventurer, a.soldier],
+                worksForThiefs: true,
             },
             {
                 name: 'humans',
                 misfits: [a.human],
+                worksForAllCriminals: true,
             },
             {
                 name: 'arcane magic',
                 misfits: [a.wizard],
                 powerFits: [a.cleric, a.knight, a.barbarian],
+                worksForAllCriminals: true,
             },
             {
                 name: 'tieflings',
                 powerFits: [a.cleric, a.human],
                 misfits: [a.tiefling],
+                worksForAssasines: true,
             },
             {
                 name: 'drows',
                 powerFits: [a.elf, a.dwarf, a.gnome, a.human],
                 misfits: [a.drow],
+                worksForAssasines: true,
             },
             {
                 name: 'small guests',
                 powerFits: [a.barbarian],
                 misfits: [a.halfling, a.gnome],
+                worksForAssasines: true,
             },
             {
                 name: 'poachers and trophy hunters',
@@ -61,6 +69,7 @@ export const bartenderOpinions: ImpressionIdea[] = [
             {
                 name: 'unpolite guests',
                 powerFits: [a.rich, a.wealthy, a.cleric],
+                worksForBrothel: true,
             },
             {
                 name: 'guests without honor',
@@ -70,6 +79,7 @@ export const bartenderOpinions: ImpressionIdea[] = [
                 name: 'poor-looking guests',
                 powerFits: [a.wealthy, a.rich],
                 misfits: [a.poor],
+                worksForBrothel: true,
             },
             {
                 name: 'fancy-dressed guests',
@@ -81,6 +91,7 @@ export const bartenderOpinions: ImpressionIdea[] = [
                     a.barbarian,
                 ],
                 misfits: [a.rich],
+                worksForAssasines: true,
             },
             {
                 name: 'uncultivated guests',
@@ -96,11 +107,13 @@ export const bartenderOpinions: ImpressionIdea[] = [
                 name: 'weak-looking guests',
                 powerFits: [a.barbarian, a.soldier, a.knight, a.assasine],
                 misfits: [a.wizard, a.druid],
+                worksForAssasines: true,
             },
             {
                 name: 'beardy guests',
                 powerFits: [a.elf, a.bard],
                 misfits: [a.dwarf, a.barbarian],
+                worksForBrothel: true,
             },
             {
                 name: 'rich-looking guests',
@@ -116,11 +129,13 @@ export const bartenderOpinions: ImpressionIdea[] = [
                 name: 'adventurers',
                 classRange: [],
                 powerFits: [a.rich, a.wealthy],
+                worksForAllCriminals: true,
             },
             {
                 name: 'weapons in his tavern',
                 classRange: [],
                 powerFits: [a.modest, a.wealthy, a.poor],
+                worksForBrothel: true,
             },
         ],
         Noticable.bartender,
@@ -144,31 +159,38 @@ export const bartenderOpinions: ImpressionIdea[] = [
                 name: 'beardy guests',
                 powerFits: [a.dwarf, a.wizard],
                 misfits: [a.elf],
+                worksForAllCriminals: true,
             },
             {
                 name: 'well-dressed guests',
                 powerFits: [a.elf, a.bard, a.prostitute, a.rich, a.wealthy],
                 misfits: [a.barbarian, a.dwarf, a.assasine],
+                worksForBrothel: true,
             },
             {
                 name: 'muscled guests',
                 powerFits: [a.barbarian, a.assasine, a.soldier],
                 misfits: [a.wizard, a.cleric],
+                worksForAssasines: true,
             },
             {
                 name: 'users of magic',
                 misfits: [a.barbarian],
                 powerFits: [a.wizard, a.adventurer],
+                worksForThiefs: true,
             },
             {
                 name: 'tieflings',
                 misfits: [a.cleric, a.human],
                 powerFits: [a.tiefling],
+                worksForThiefs: true,
             },
             {
                 name: 'small guests',
                 misfits: [a.drow],
                 powerFits: [a.gnome, a.halfling],
+                worksForThiefs: true,
+                worksForBrothel: true,
             },
             {
                 name: 'animal-loving guests',
@@ -177,6 +199,8 @@ export const bartenderOpinions: ImpressionIdea[] = [
             {
                 name: 'sophisticated guests',
                 powerFits: [a.rich, a.wealthy, a.knight],
+                worksForThiefs: true,
+                worksForBrothel: true,
             },
             {
                 name: 'virtous guests',
@@ -187,30 +211,38 @@ export const bartenderOpinions: ImpressionIdea[] = [
                 name: 'poor-looking guests',
                 powerFits: [a.cleric, a.poor, a.modest],
                 misfits: [a.rich, a.wealthy],
+                worksForThiefs: true,
             },
             {
                 name: 'fancy-dressed guests',
                 powerFits: [a.rich, a.bard, a.elf],
                 misfits: [a.poor, a.modest, a.knight],
+                worksForBrothel: true,
             },
             {
                 name: 'guests with manners',
                 powerFits: [a.wealthy, a.rich, a.knight],
                 misfits: [a.poor, a.barbarian, a.soldier],
+                worksForThiefs: true,
+                worksForBrothel: true,
             },
             {
                 name: 'wild-looking guests',
                 needsOne: [a.barbarian, a.druid, a.assasine],
+                worksForAssasines: true,
             },
             {
                 name: 'rich-looking guests',
                 powerFits: [a.wealthy, a.rich],
                 misfits: [a.cleric, a.modest, a.poor],
+                worksForBrothel: true,
+                worksForThiefs: true,
             },
             {
                 name: 'foreigners',
                 powerFits: [a.haven, a.adventurer],
                 misfits: [a.village],
+                worksForBrothel: true,
             },
             {
                 name: 'story-sharing adventurers',
@@ -223,6 +255,7 @@ export const bartenderOpinions: ImpressionIdea[] = [
                     a.cleric,
                     a.knight,
                 ],
+                worksForBrothel: true,
             },
             {
                 name: 'experienced warriors',
@@ -234,6 +267,8 @@ export const bartenderOpinions: ImpressionIdea[] = [
                     a.dwarf,
                 ],
                 misfits: [a.wizard, a.bard],
+                worksForBrothel: true,
+                worksForAssasines: true,
             },
             {
                 name: 'cheerful guests',
@@ -249,6 +284,18 @@ export const bartenderOpinions: ImpressionIdea[] = [
                 name: 'guests who play instruments',
                 powerFits: [a.elf, a.human, a.bard],
                 misfits: [a.drow],
+            },
+            {
+                name: 'guests who grew up in a city',
+                needs: [a.city],
+            },
+            {
+                name: 'guests who grew up in a village',
+                needsOne: [a.halfling, a.village],
+            },
+            {
+                name: 'guests who grew up in a harbor city',
+                needs: [a.haven],
             },
         ],
         Noticable.bartender,
