61ef78f7a8ba138865748a0a2c2421969ff6b8e7
knoffi <matthiastutor@outlook.de>
2021-08-31 00:10:04 +0200

    ADD #66 more knight individuals

--- scenes/questScene/impressions/individuals/knightIndividuals.ts
+++ scenes/questScene/impressions/individuals/knightIndividuals.ts
@@ -33,6 +33,54 @@ export const knightIndividuals: ImpressionIdea[] = [
         defaultPowerFitConcepts.nameWithAdditions
     ),
     new ImpressionIdea(
+        { name: 'A grey-haired paladin is ' },
+        [...grownWarriorActions],
+        Noticable.someCustomers,
+        undefined,
+        undefined,
+        defaultPowerFitConcepts.nameWithAdditions
+    ),
+    new ImpressionIdea(
+        { name: 'A royal knight is ' },
+        [...grownWarriorActions],
+        Noticable.someCustomers,
+        undefined,
+        undefined,
+        defaultPowerFitConcepts.nameWithAdditions
+    ),
+    new ImpressionIdea(
+        { name: 'A well-known lord is ' },
+        [...grownWarriorActions],
+        Noticable.someCustomers,
+        undefined,
+        undefined,
+        defaultPowerFitConcepts.nameWithAdditions
+    ),
+    new ImpressionIdea(
+        { name: 'A royal knight is ' },
+        [...grownWarriorActions],
+        Noticable.someCustomers,
+        undefined,
+        undefined,
+        defaultPowerFitConcepts.nameWithAdditions
+    ),
+    new ImpressionIdea(
+        { name: 'A two royal guards are is ' },
+        [...grownWarriorActions],
+        Noticable.someCustomers,
+        undefined,
+        undefined,
+        defaultPowerFitConcepts.nameWithAdditions
+    ),
+    new ImpressionIdea(
+        { name: 'A female paladin is ' },
+        [...grownWarriorActions],
+        Noticable.someCustomers,
+        undefined,
+        undefined,
+        defaultPowerFitConcepts.nameWithAdditions
+    ),
+    new ImpressionIdea(
         { name: 'A shield maid warrior is ' },
         [...grownWarriorActions],
         Noticable.someCustomers,
@@ -41,7 +89,7 @@ export const knightIndividuals: ImpressionIdea[] = [
         defaultPowerFitConcepts.nameWithAdditions
     ),
     new ImpressionIdea(
-        { name: 'A blond woman with a sword is ' },
+        { name: 'A blond woman is ' },
         [...grownWarriorActions],
         Noticable.someCustomers,
         undefined,
@@ -64,4 +112,8 @@ export const knightIndividuals: ImpressionIdea[] = [
         undefined,
         defaultPowerFitConcepts.nameWithAdditions
     ),
+
+    // key max 2 : singleWarrior , key max 1: groupOfWarriors, key max 1: protectedByKnight ?
+    // further ideas: lady/lord/child protected/accompanied by a knight/paladin
+    // further ideas: bard who hans out with knights
 ];
