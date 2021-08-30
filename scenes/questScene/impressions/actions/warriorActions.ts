de99eff2a48d7c722c4d3e5c46e2a1fb9c8a56a4
knoffi <matthiastutor@outlook.de>
2021-08-30 23:58:23 +0200

    ADD #66 squire actions

--- scenes/questScene/impressions/actions/warriorActions.ts
+++ scenes/questScene/impressions/actions/warriorActions.ts
@@ -1,7 +1,7 @@
 import { DescriptionAsset } from '../../../../classes/idea/DescriptionAsset';
 
 export const grownWarriorActions: DescriptionAsset[] = [
-    { name: 'telling stories about a past crusade' },
+    { name: 'telling stories about a past siege' },
     { name: 'telling stories about a battle in the mountains' },
     { name: 'telling stories about a war in the highlands' },
     { name: 'telling stories about a battle in the desert' },
@@ -9,19 +9,24 @@ export const grownWarriorActions: DescriptionAsset[] = [
     { name: 'telling stories about a war on a far-away continent' },
     { name: 'telling stories about the war against the drow' },
     { name: 'telling stories about the war against the dwarves' },
-    { name: 'making a toast to a fallen comrade' },
     { name: 'making a toast to a fallen squire' },
+    { name: 'making a toast to a fallen comrade' },
     { name: 'making a toast to the royal family' },
     { name: 'making a toast to the gods of justice' },
     { name: 'making a toast to the gods of honor' },
     { name: 'making a toast to his fallen stallion' },
-    { name: 'polishing a big claymore' },
-    { name: 'polishing a shining armor' },
     { name: 'exchanging war stories at the table' },
     { name: 'exchanging heroic stories with other guests' },
     { name: 'having a drink contest with his squire' },
     { name: 'searching his drunken squire' },
     { name: 'resting from a previous battle' },
+];
+
+export const warriorActions: DescriptionAsset[] = [
+    { name: 'making a toast to a fallen comrade' },
+    { name: 'polishing a big claymore' },
+    { name: 'polishing a shining armor' },
     { name: 'transporting secret battle plans' },
     { name: 'showing his muscles in front of a woman' },
+    { name: 'finishing a drink and then burping really loud' },
 ];
--- scenes/questScene/impressions/individuals/knightIndividuals.ts
+++ scenes/questScene/impressions/individuals/knightIndividuals.ts
@@ -1,11 +1,27 @@
 import { ImpressionIdea } from '../../../../classes/idea/ImpressionIdea';
 import { Noticable } from '../../../../classes/idea/Noticable';
 import { defaultPowerFitConcepts } from '../../../../classes/idea/powerFitConcepts/powerFitConcepts';
-import { grownWarriorActions } from '../actions/warriorActions';
+import { grownWarriorActions, warriorActions } from '../actions/warriorActions';
 
 export const knightIndividuals: ImpressionIdea[] = [
     new ImpressionIdea(
         { name: 'A knight is ' },
+        [...grownWarriorActions],
+        Noticable.someCustomers,
+        undefined,
+        undefined,
+        defaultPowerFitConcepts.nameWithAdditions
+    ),
+    new ImpressionIdea(
+        { name: 'A squire is ' },
+        [...warriorActions],
+        Noticable.someCustomers,
+        undefined,
+        undefined,
+        defaultPowerFitConcepts.nameWithAdditions
+    ),
+    new ImpressionIdea(
+        { name: 'A sturdy warrior is ' },
         [...grownWarriorActions],
         Noticable.someCustomers,
         undefined,
