0e8b63f6f1b405584a82e198f9ff31ae608d2f7c
knoffi <matthiastutor@outlook.de>
2021-09-01 00:55:55 +0200

    ADD #66 warrior actions refinement

--- scenes/questScene/impressions/actions/brothelActions.ts
+++ scenes/questScene/impressions/actions/brothelActions.ts
@@ -5,13 +5,16 @@ export const brothelActions: DescriptionAsset[] = [
     {
         name: 'flirting with a prostitute',
         needs: [a.prostitute],
+        worksForBrothel: true,
     },
     {
         name: 'badly flirting with a prostitute',
         needs: [a.prostitute],
+        worksForBrothel: true,
     },
     {
         name: 'getting slapped by a prostitute',
         needs: [a.prostitute],
+        worksForBrothel: true,
     },
 ];
