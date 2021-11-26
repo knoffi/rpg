f3a6f8c1ddf34edcb1ae4a71d92782b4f89e1740
knoffi <matthiastutor@outlook.de>
2021-11-26 22:59:31 +0100

    ADD #102 metro bundler ignores test folder ::: non-production tests increase bundler size

---
+++ metro.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    /* general options */
+  
+    resolver: {
+        blockList:/logicTests/g
+    },
+    transformer: {
+      /* transformer options */
+    },
+    serializer: {
+      /* serializer options */
+    },
+    server: {
+      /* server options */
+    }
+  };
