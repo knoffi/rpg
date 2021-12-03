0ea191b52c1902a220ef6bc05c3cadef45c0df4f
knoffi <matthiastutor@outlook.de>
2021-12-03 22:09:27 +0100

    ADD #102 tests for pattern bonus by addition

--- logicTests/ideaMethods.ts
+++ logicTests/ideaMethods.ts
@@ -1,6 +1,14 @@
 import { expect } from 'chai';
+import { DescriptionAsset } from '../classes/idea/DescriptionAsset';
 import { Idea } from '../classes/idea/Idea';
+import { ImpressionIdea } from '../classes/idea/ImpressionIdea';
+import { Noticable } from '../classes/idea/Noticable';
 import { Pattern } from '../classes/idea/Patterns/Pattern';
+import { defaultPowerFitConcepts } from '../classes/idea/powerFitConcepts/powerFitConcepts';
+const emptyDescriptionAsset: DescriptionAsset = {
+    name: '',
+    worksForAllCriminals: true,
+};
 describe('Idea method tests', () => {
     it('main pattern bonus with empty additions', () => {
         const withoutPatternBonus = new Idea(
@@ -88,5 +96,182 @@ describe('Idea method tests', () => {
         expect(withPatternBonus).to.be.greaterThan(withoutPatternBonus);
         expect(withPatternBonus).to.be.greaterThan(withWrongPatternBonus);
         expect(withWrongPatternBonus).to.be.eql(withoutPatternBonus);
+    });
+    it('pattern bonus with multiple patterns', () => {
+        const littleFinger = new ImpressionIdea(
+            {
+                name: 'Knows a secret way into the royal castle',
+                patterns: [Pattern.BARTENDER_Kleinfinger],
+            },
+            [emptyDescriptionAsset],
+            Noticable.bartender
+        ).getFitLevelForTavern(
+            {},
+            () => false,
+            undefined,
+            undefined,
+            () => false,
+            () => false,
+            [
+                Pattern.BARTENDER_UncleBen,
+                Pattern.BARTENDER_UncleBen,
+                Pattern.BARTENDER_Kleinfinger,
+            ]
+        );
+        const uncleBen = new ImpressionIdea(
+            {
+                name: 'Very friendly and responsible',
+                patterns: [Pattern.BARTENDER_UncleBen],
+            },
+            [emptyDescriptionAsset],
+            Noticable.bartender
+        ).getFitLevelForTavern(
+            {},
+            () => false,
+            undefined,
+            undefined,
+            () => false,
+            () => false,
+            [
+                Pattern.BARTENDER_UncleBen,
+                Pattern.BARTENDER_UncleBen,
+                Pattern.BARTENDER_Kleinfinger,
+            ]
+        );
+
+        expect(uncleBen).to.be.greaterThan(littleFinger);
+    });
+    it('pattern bonus for impressions', () => {
+        const littleFinger = new ImpressionIdea(
+            {
+                name: 'Knows a secret way into the royal castle',
+                patterns: [Pattern.BARTENDER_Kleinfinger],
+            },
+            [emptyDescriptionAsset],
+            Noticable.bartender
+        ).getFitLevelForTavern(
+            {},
+            () => false,
+            undefined,
+            undefined,
+            () => false,
+            () => false,
+            [Pattern.BARTENDER_UncleBen]
+        );
+        const uncleBen = new ImpressionIdea(
+            {
+                name: 'Very friendly and responsible',
+                patterns: [Pattern.BARTENDER_UncleBen],
+            },
+            [emptyDescriptionAsset],
+            Noticable.bartender
+        ).getFitLevelForTavern(
+            {},
+            () => false,
+            undefined,
+            undefined,
+            () => false,
+            () => false,
+            [Pattern.BARTENDER_UncleBen]
+        );
+
+        expect(uncleBen).to.be.greaterThan(littleFinger);
+    });
+    it('pattern bonus for equlibirium case', () => {
+        const littleFinger = new ImpressionIdea(
+            {
+                name: 'Knows a secret way into the royal castle',
+                patterns: [Pattern.BARTENDER_Kleinfinger],
+            },
+            [emptyDescriptionAsset],
+            Noticable.bartender
+        ).getFitLevelForTavern(
+            {},
+            () => false,
+            undefined,
+            undefined,
+            () => false,
+            () => false,
+            [Pattern.BARTENDER_UncleBen, Pattern.BARTENDER_Kleinfinger]
+        );
+        const uncleBen = new ImpressionIdea(
+            {
+                name: 'Very friendly and responsible',
+                patterns: [Pattern.BARTENDER_UncleBen],
+            },
+            [emptyDescriptionAsset],
+            Noticable.bartender
+        ).getFitLevelForTavern(
+            {},
+            () => false,
+            undefined,
+            undefined,
+            () => false,
+            () => false,
+            [Pattern.BARTENDER_UncleBen, Pattern.BARTENDER_Kleinfinger]
+        );
+
+        expect(uncleBen).to.equal(littleFinger);
+    });
+    it('pattern bonus for addition', () => {
+        const uncleBen = new ImpressionIdea(
+            {
+                name: '',
+            },
+            [{ name: 'UB', patterns: [Pattern.BARTENDER_UncleBen] }],
+            Noticable.bartender,
+            undefined,
+            undefined,
+            undefined,
+            defaultPowerFitConcepts.harmony
+        ).getFitLevelForTavern(
+            {},
+            () => false,
+            undefined,
+            undefined,
+            () => false,
+            () => false,
+            [Pattern.BARTENDER_UncleBen]
+        );
+        const littleFinger = new ImpressionIdea(
+            {
+                name: '',
+            },
+            [{ name: 'LF', patterns: [Pattern.BARTENDER_Kleinfinger] }],
+            Noticable.bartender,
+            undefined,
+            undefined,
+            undefined,
+            defaultPowerFitConcepts.harmony
+        ).getFitLevelForTavern(
+            {},
+            () => false,
+            undefined,
+            undefined,
+            () => false,
+            () => false,
+            [Pattern.BARTENDER_UncleBen]
+        );
+        const nobody = new ImpressionIdea(
+            {
+                name: '',
+            },
+            [{ name: 'NB', patterns: [] }],
+            Noticable.bartender,
+            undefined,
+            undefined,
+            undefined,
+            defaultPowerFitConcepts.harmony
+        ).getFitLevelForTavern(
+            {},
+            () => false,
+            undefined,
+            undefined,
+            () => false,
+            () => false,
+            [Pattern.BARTENDER_UncleBen]
+        );
+        expect(uncleBen).to.be.greaterThan(littleFinger);
+        expect(littleFinger).to.equal(nobody);
     });
 });
