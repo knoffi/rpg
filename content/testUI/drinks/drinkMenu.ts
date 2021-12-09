3d9b6bd57984e574a1443037aa2fd17f06a6f1cc
knoffi <matthiastutor@outlook.de>
2021-12-09 18:45:20 +0100

    FIX #99 content for UI tests and Unit testing

--- content/testUI/drinks/drinkMenu.ts
+++ content/testUI/drinks/drinkMenu.ts
@@ -4,11 +4,12 @@ import { lagers } from './beer/lagers';
 import { lemonades } from './softDrinks/lemonade';
 import { water } from './softDrinks/water';
 import { rum } from './spirits/rum';
+import { meads } from './wine/meads';
 import { redWines } from './wine/redWines';
 
 export const drinkMenu: IDrinkMenu[] = [
     { category: Drinkable.beer, ideas: [...lagers] },
-    { category: Drinkable.wine, ideas: [...redWines] },
+    { category: Drinkable.wine, ideas: [...redWines, ...meads] },
     { category: Drinkable.spirit, ideas: [...rum] },
     { category: Drinkable.lemonade, ideas: [...lemonades, ...water] },
 ];
