ede1458bc7facbc240e02a1ee0d2478004822123
knoffi <matthiastutor@outlook.de>
2022-02-14 16:41:10 +0100

    TEST #161 validation of dishes

--- logicTests/validation.ts
+++ logicTests/validation.ts
@@ -1,6 +1,12 @@
 import { expect } from 'chai';
 import { association } from '../classes/association';
+import { FantasyKeys } from '../classes/contentCreator/FantasKeys';
 import { DBValidator } from '../classes/database/Validator/DBValidator';
+import { AssetKey } from '../classes/idea/AssetKey/AssetKey';
+import { Pattern } from '../classes/idea/Patterns/Pattern';
+import { Eatable } from '../classes/TavernProduct';
+import { WeServe } from '../editNavigator/WeServe';
+import { Offer } from '../scenes/menuScene/Offer';
 import { MinimalTavernData } from './../mainNavigator/TavernData';
 import { Constants } from './constants/Constants';
 const wrap = (data: any, keyToRemove?: string) => {
@@ -24,7 +30,7 @@ describe('DB validation', () => {
             impression: [],
         };
         const json = JSON.stringify(wrap(incomplete));
-        const construction = validator.construct(json, 'tavern');
+        const construction = validator.parse(json, 'tavern');
         expect(construction).to.be.undefined;
     });
     it('unpure tavern', () => {
@@ -47,7 +53,7 @@ describe('DB validation', () => {
             [unusedKey]: 'evil code is evil',
         };
         const json = JSON.stringify(wrap(unpure));
-        const construction = validator.construct(json, 'tavern');
+        const construction = validator.parse(json, 'tavern');
         expect(construction).to.eql(wrap(unpure, unusedKey));
     });
     it('pure tavern', () => {
@@ -68,7 +74,56 @@ describe('DB validation', () => {
             universe: Constants.universe,
         };
         const json = JSON.stringify(wrap(pure));
-        const construction = validator.construct(json, 'tavern');
+        const construction = validator.parse(json, 'tavern');
+        expect(construction).to.eql(wrap(pure));
+    });
+    it('incomplete dish', () => {
+        const incomplete: Partial<Offer> = {
+            category: Eatable.dessert,
+            isAbout: WeServe.food,
+            name: 'Tasty stuff',
+        };
+        const json = JSON.stringify(wrap(incomplete));
+        const construction = validator.parse(json, WeServe.food);
+        expect(construction).to.be.undefined;
+    });
+    it('unpure dish', () => {
+        const unusedKey = 'evilPollution';
+        const unpure: Offer & { [unusedKey]: string } = {
+            name: 'Tomatoe Soup',
+            category: Eatable.sideDish,
+            isAbout: WeServe.food,
+            description: 'Super tasty',
+            impliedPatterns: [],
+            income: association.rich,
+            isUserMade: true,
+            keys: { main: [AssetKey.SMALL_DISH_soup], addition: [] },
+            patterns: [Pattern.BARTENDER_UncleBen],
+            price: 15,
+            universe: FantasyKeys.testing,
+
+            [unusedKey]: 'evil code is evil',
+        };
+        const json = JSON.stringify(wrap(unpure));
+        const construction = validator.parse(json, WeServe.food);
+        expect(construction).to.eql(wrap(unpure, unusedKey));
+    });
+    it('pure dish', () => {
+        const pure: Offer = {
+            name: 'Tomatoe Soup',
+            category: Eatable.sideDish,
+            isAbout: WeServe.food,
+            description: 'Super tasty',
+            impliedPatterns: [],
+            income: association.rich,
+            isUserMade: true,
+            keys: { main: [AssetKey.SMALL_DISH_soup], addition: [] },
+            patterns: [Pattern.BARTENDER_UncleBen],
+            price: 15,
+            universe: FantasyKeys.testing,
+        };
+        const json = JSON.stringify(wrap(pure));
+        const construction = validator.parse(json, WeServe.food);
         expect(construction).to.eql(wrap(pure));
     });
 });
