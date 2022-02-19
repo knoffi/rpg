707d24729f05ddc0a3444cce113c04c9c9eb3aa0
knoffi <matthiastutor@outlook.de>
2022-02-19 12:48:21 +0100

    ADD #172 share pdf generated from tavern

--- mainNavigator/mainNavigatorFunctions.ts
+++ mainNavigator/mainNavigatorFunctions.ts
@@ -1,4 +1,5 @@
-import { Share } from 'react-native';
+import * as Print from 'expo-print';
+import { shareAsync } from 'expo-sharing';
 import { DismissHandler } from '../classes/dismissHandler/dismissHandler';
 import { KeyHandler } from '../classes/keyHandler/KeyHandler';
 import { PatternHandler } from '../classes/patternHandler/PatternHandler';
@@ -47,8 +48,16 @@ export const getNewTracker = (change: TavernChange, old: ContentTracker) => {
     return newTracker;
 };
 
-const offerLine = (offer: Offer): string => {
-    return '   ' + offer.name + '  ' + offer.price;
+const offerLine = (offer: Offer, currency: string): string => {
+    return (
+        '<text>' +
+        offer.name +
+        ':   <i>' +
+        offer.price +
+        ' ' +
+        currency +
+        '</i></text><br/><br/>'
+    );
 };
 
 const offersByCategory = (
@@ -75,55 +84,130 @@ const offersByCategory = (
 
 const textForCategory = (
     category: Drinkable | Eatable,
-    offerMap: Map<Drinkable | Eatable, Offer[]>
+    offerMap: Map<Drinkable | Eatable, Offer[]>,
+    currency: string
 ) => {
     const offers = offerMap.get(category);
     const noOffersForCategory = !offers || offers.length === 0;
     if (noOffersForCategory) {
         return '';
     } else {
-        const title = '\n\n' + category.toUpperCase() + '\n\n';
+        const title =
+            '<br/><h2> ' +
+            emojis.get(category) +
+            emojis.get(category) +
+            category +
+            emojis.get(category) +
+            emojis.get(category) +
+            ' </h2>';
         const menu = offers.reduce(
-            (prev, cur) => prev + offerLine(cur) + '\n',
+            (prev, cur) => prev + offerLine(cur, currency),
             ''
         );
         return title + menu;
     }
 };
 
-const constructMessage = (
-    tavern: Pick<MinimalTavernData, 'name' | WeServe.drinks | WeServe.food>
+const wrapInBodyTag = (view: string) => {
+    return `<body style="text-align: center;">` + view + `</body>`;
+};
+
+const constructHTML = (
+    tavern: Pick<
+        MinimalTavernData,
+        'name' | WeServe.drinks | WeServe.food | 'prices'
+    >
 ): string => {
-    const greeting = '*| ' + tavern.name + ' |*\n\n\n';
+    const headTag =
+        '<head><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" /></head>';
+    const nameTitle = '<h1>' + tavern.name.toUpperCase() + '</h1><br/><br/>';
     const chapterMap = offersByCategory(tavern);
-    const foodTitle = '*FOOD*';
-    const foodPage =
-        foodTitle +
-        Object.values(Eatable).reduce(
-            (text, category) => text + textForCategory(category, chapterMap),
-            ''
-        ) +
-        '\n';
-    const drinkTitle = '*DRINKS*';
-    const drinkPage =
-        drinkTitle +
-        Object.values(Drinkable).reduce(
-            (text, category) => text + textForCategory(category, chapterMap),
-            ''
-        ) +
-        '\n';
-    return greeting + foodPage + drinkPage;
+    const foodPage = Object.values(Eatable)
+        .map((category) =>
+            textForCategory(category, chapterMap, tavern.prices.currency)
+        )
+        .join('');
+    const drinkPage = Object.values(Drinkable)
+        .map((category) =>
+            textForCategory(category, chapterMap, tavern.prices.currency)
+        )
+        .join('');
+    const body = wrapInBodyTag(nameTitle + foodPage + drinkPage);
+    return headTag + body;
 };
 
 export const openShare = async (
-    tavern: Pick<MinimalTavernData, 'name' | WeServe.drinks | WeServe.food>
+    tavern: Pick<
+        MinimalTavernData,
+        'name' | WeServe.drinks | WeServe.food | 'prices'
+    >
 ) => {
     try {
-        const message = constructMessage(tavern);
-        const result = await Share.share({
-            message,
+        const { uri } = await Print.printToFileAsync({
+            html: constructHTML(tavern),
         });
+        await shareAsync(uri, { UTI: '.pdf', mimeType: 'application/pdf' });
     } catch (error) {
         console.log(error);
     }
 };
+
+const emojis = new Map<Eatable | Drinkable, string>([
+    [Drinkable.wine, ' 🍷 '],
+    [Drinkable.beer, ' 🍺 '],
+    [Eatable.dessert, ' 🍰 '],
+    [Eatable.breakfast, ' 🍞 '],
+    [Eatable.mainDish, ' 🍝 '],
+    [Eatable.sideDish, ' 🥗 '],
+    [Drinkable.lemonade, ' 🥛 '],
+    [Drinkable.spirit, ' 🥃 '],
+]);
+// const getHTML = (
+//     offer: Pick<Offer, 'name' | 'price'>,
+//     category: Drinkable | Eatable,
+//     currency: string
+// ): string => {
+//     return (
+//         '<h2> ' +
+//         emojis.get(category) +
+//         emojis.get(category) +
+//         category +
+//         emojis.get(category) +
+//         emojis.get(category) +
+//         ' </h2><text>' +
+//         offer.name +
+//         ':   <i>' +
+//         offer.price +
+//         ' ' +
+//         currency +
+//         '</i></text><br/><br/>'
+//     );
+// };
+// const html =
+//     `
+// <html>
+//   <head>
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+//   </head>
+//   <body style="text-align: center;">` +
+//     `<h1>The Magical Mug</h1><br/><br/>` +
+//     getHTML({ name: 'Porridge', price: 10 }, Eatable.breakfast, 'copper') +
+//     getHTML({ name: 'Apple Pie', price: 15 }, Eatable.dessert, 'copper') +
+//     getHTML({ name: 'Chicken Soup', price: 10 }, Eatable.sideDish, 'copper') +
+//     getHTML({ name: 'Roast Beef', price: 24 }, Eatable.mainDish, 'copper') +
+//     '<br/><br/><text style="white-space: nowrap;">' +
+//     getHTML({ name: 'Water', price: 2 }, Drinkable.lemonade, 'copper') +
+//     getHTML(
+//         { name: "Baselbruck's Beer", price: 10 },
+//         Drinkable.beer,
+//         'copper'
+//     ) +
+//     getHTML({ name: 'Rose Mead', price: 15 }, Drinkable.wine, 'copper') +
+//     getHTML(
+//         { name: 'Van Tasty Whiskey', price: 20 },
+//         Drinkable.spirit,
+//         'copper'
+//     ) +
+//     `</text></body>
+// </html>
+// `;
