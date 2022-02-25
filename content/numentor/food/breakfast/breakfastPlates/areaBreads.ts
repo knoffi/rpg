c9867e05262d7b7a026aafcedb135417e2614837
knoffi <matthiastutor@outlook.de>
2022-02-25 22:52:47 +0100

    ADD #146 snadwiches

--- content/numentor/food/breakfast/breakfastPlates/areaBreads.ts
+++ content/numentor/food/breakfast/breakfastPlates/areaBreads.ts
@@ -3,6 +3,14 @@ import { DescriptionAsset } from '../../../../../classes/idea/DescriptionAsset';
 
 const a = association;
 export const areaBreads: DescriptionAsset[] = [
+    {
+        name: 'Pancakes',
+        landRange: [a.village, a.haven, a.city],
+    },
+    {
+        name: 'Millet Pancakes',
+        landRange: [a.village, a.forest, a.mountain],
+    },
     { name: 'Bread Rolls', misfits: [a.tropical, a.desert] },
     {
         name: 'Whole-Grain Bread',
@@ -13,25 +21,29 @@ export const areaBreads: DescriptionAsset[] = [
         landRange: [a.village, a.forest, a.mountain, a.haven, a.city],
     },
     {
-        name: 'Millet Pancakes',
-        landRange: [a.village, a.forest, a.mountain],
+        name: 'Banana Bread',
+        needs: [a.tropical],
     },
     {
-        name: 'Pancakes',
-        landRange: [a.village, a.haven, a.city],
+        name: 'Pineapple Bread',
+        needs: [a.tropical],
+    },
+    {
+        name: 'Corn Bread',
+        needs: [a.tropical],
     },
     {
         name: 'Flatbread',
-        landRange: [a.desert, a.tropical, a.mountain, a.haven, a.village],
+        landRange: [a.desert, a.mountain, a.haven, a.village],
     },
     { name: 'Baguette', landRange: [a.city, a.village] },
-    { name: 'White Bread', landRange: [a.city, a.haven, a.tropical] },
+    { name: 'White Bread', landRange: [a.city, a.haven] },
     {
         name: 'Pita Bread',
-        landRange: [a.desert, a.mountain, a.haven, a.tropical],
+        landRange: [a.desert, a.mountain, a.haven],
     },
     {
         name: 'Sourdough Bread',
-        landRange: [a.desert, a.mountain, a.haven, a.tropical],
+        landRange: [a.desert, a.mountain, a.haven],
     },
 ];
--- content/numentor/food/smallDish/bread.ts
+++ content/numentor/food/smallDish/bread.ts
@@ -36,4 +36,154 @@ export const carbonStarter: DishIdea[] = [
         'default',
         Eatable.sideDish
     ),
+    new DishIdea(
+        {
+            mainIng: {
+                name: 'Sandwich',
+                key: AssetKey.SMALL_DISH_carbon,
+                misfits: [a.desert, a.tropical],
+                incomeRange: [a.poor, a.modest],
+            },
+            firstSideDishes: [
+                { name: 'Bread Rolls', landRange: [a.city, a.haven] },
+                {
+                    name: 'Whole-Grain Bread',
+                    landRange: [a.village, a.mountain],
+                },
+                {
+                    name: 'Multigrain Bread',
+                    landRange: [a.village, a.forest],
+                },
+                {
+                    name: 'White Bread',
+                    landRange: [a.city, a.haven],
+                },
+                {
+                    name: 'Sourdough Bread',
+                    landRange: [a.city, a.village],
+                },
+                {
+                    name: 'Rye Bread',
+                    landRange: [a.mountain, a.city],
+                },
+                {
+                    name: 'Potato Bread',
+                    landRange: [a.village, a.underdark],
+                },
+                {
+                    name: 'Pumpkin Bread',
+                    landRange: [a.forest, a.village],
+                },
+            ],
+            secondSideDishes: [
+                {
+                    name: 'with Butter',
+                    needs: [a.poor],
+                    misfits: [a.haven],
+                },
+                {
+                    name: 'with Oily Anchovies',
+                    needs: [a.poor, a.haven],
+                },
+                {
+                    name: 'with Butter & Smoked Ham',
+                    landRange: [a.mountain, a.city, a.underdark],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Butter & Ham',
+                    landRange: [a.village, a.city],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Butter & Cheese',
+                    landRange: [a.village, a.forest],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Sausage Slices',
+                    needs: [a.mountain],
+                },
+                {
+                    name: 'with Blood Sausage',
+                    needs: [a.mountain],
+                },
+                {
+                    name: 'with Liver Sausage',
+                    needs: [a.mountain],
+                },
+                {
+                    name: 'with Salmon and Onions',
+                    needs: [a.haven],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Tuna and Onions',
+                    needs: [a.haven],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Hard-Boiled Egg Slices',
+                    misfits: [a.haven],
+                },
+                {
+                    name: 'with Cheese & Pickles',
+                    needs: [a.city],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Cheese, Bacon and Pickles',
+                    needs: [a.city],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Cheese, Mole Bacon and Pickles',
+                    needs: [a.underdark],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Cheese, a Fried Egg and Pickles',
+                    needs: [a.city],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with various Mushrooms and Onions',
+                    needsOne: [a.underdark, a.forest],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'topped with a Fried Egg and Pickles',
+                    needs: [a.city],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Butter and Beef Cuts',
+                    needs: [a.city],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Butter and Lamb Cuts',
+                    needs: [a.mountain],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Butter and Pork Cuts',
+                    needs: [a.village],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'with Butter and Slices of Boar Meat',
+                    needs: [a.forest],
+                    misfits: [a.poor],
+                },
+                {
+                    name: 'topped with Stink Fungus',
+                    needsOne: [a.underdark, a.forest],
+                    misfits: [a.poor],
+                },
+            ],
+        },
+        0.9,
+        Eatable.sideDish
+    ),
 ];
