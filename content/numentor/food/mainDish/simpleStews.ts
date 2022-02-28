0c4e02a2428197ea79143c6ca8e8955fd94d17f2
knoffi <matthiastutor@outlook.de>
2022-02-28 11:41:02 +0100

    FIX #99 simple stew fitting

--- content/numentor/food/mainDish/simpleStews.ts
+++ content/numentor/food/mainDish/simpleStews.ts
@@ -2,134 +2,133 @@ import { association } from '../../../../classes/association';
 import { DishIdea } from '../../../../classes/idea/DishIdea';
 import { Eatable } from '../../../../classes/TavernProduct';
 
-const STEW_PRICE_FACTOR = 0.7;
 const a = association;
-export const leftoverStew =
-    //TODO: an mainIngredient sollte schon erkannt werden können, ob es ein Produkt der Familie im Essen gibt. Beschleunigt alles ein wenig und reicht normalerWeise (wenn nicht, wird es später auch nicht so schlimm sein)
-    new DishIdea(
-        {
-            mainIng: {
-                name: 'Leftover Stew',
-                incomeRange: [a.poor, a.modest],
-            },
-            firstSideDishes: [
-                {
-                    name: ' with Chicken Legs',
-                },
-                {
-                    name: ' with Beef Chunks',
-                },
-                {
-                    name: ' with Pork Chunks',
-                    landRange: [a.city, a.village, a.tropical],
-                },
-                {
-                    name: ' with Boar Chunks',
-                    landRange: [a.forest, a.mountain],
-                },
-                {
-                    name: ' with Chick Peas, Coriander',
-                    landRange: [a.desert],
-                },
-                {
-                    name: ' with Lamb Chunks',
-                    landRange: [a.desert, a.mountain],
-                    misfits: [a.poor],
-                },
-                {
-                    name: ' with Camel Chunks',
-                    landRange: [a.desert],
-                    misfits: [a.poor],
-                },
-                {
-                    name: ' with Beans, Onions',
-                },
-                {
-                    name: ' with Dried Squid',
-                    needs: [a.haven],
-                    misfits: [a.poor],
-                },
-                {
-                    name: ' with Pumpkin Chunks',
-                    landRange: [a.forest, a.village],
-                },
-                {
-                    name: ' with Shrimps',
-                    needs: [a.haven],
-                    incomeRange: [a.modest],
-                },
-                {
-                    name: ' with Anchovies',
-                    needs: [a.haven],
-                    incomeRange: [a.poor],
-                },
-                {
-                    name: ' with Smoked Salmon',
-                    landRange: [a.haven, a.village, a.city, a.modest],
-                },
-                {
-                    name: ' with Boar Feet',
-                    landRange: [a.village, a.forest, a.mountain],
-                    incomeRange: [a.poor],
-                },
-                {
-                    name: ' with Pig Feet',
-                    landRange: [a.village, a.city, a.forest],
-                    incomeRange: [a.poor],
-                },
-                {
-                    name: ' with Pickled Mole',
-                    landRange: [a.underdark],
-                    incomeRange: [a.modest],
-                },
-                {
-                    name: ' with Lizard Chunks',
-                    landRange: [a.underdark],
-                    incomeRange: [a.wealthy],
-                },
-                {
-                    name: ' with Minced Earth Worms',
-                    landRange: [a.underdark],
-                    incomeRange: [a.poor],
-                },
-            ],
-            secondSideDishes: [
-                {
-                    name: ' and Bread',
-                    landRange: [a.city, a.village],
-                    incomeRange: [a.poor],
-                },
-                {
-                    name: ' and Potatoes',
-                    incomeRange: [a.modest],
-                },
-                {
-                    name: ' and Cabbage',
-                    landRange: [a.forest],
-                    incomeRange: [a.poor],
-                },
-                {
-                    name: ' and Turmeric Rice',
-                    landRange: [a.desert, a.haven],
-                    incomeRange: [a.modest],
-                },
-                {
-                    name: ' and Coconut Rice',
-                    landRange: [a.tropical],
-                    incomeRange: [a.modest],
-                },
-                {
-                    name: ' and Brown Rice',
-                    landRange: [a.tropical, a.desert, a.haven],
-                    incomeRange: [a.modest],
-                },
-                {
-                    name: ' and Flat Bread',
-                    landRange: [a.tropical, a.mountain, a.desert],
-                    incomeRange: [a.poor],
-                },
-            ],
+const STEW_PRICE_FACTOR = { [a.modest]: 0.7 };
+export const leftoverStew = new DishIdea(
+    {
+        mainIng: {
+            name: 'Leftover Stew',
+            incomeRange: [a.poor, a.modest],
+            powerFits: [a.poor],
         },
-        STEW_PRICE_FACTOR,
-        Eatable.mainDish
-    );
+        firstSideDishes: [
+            {
+                name: ' with Chicken Legs',
+            },
+            {
+                name: ' with Beef Chunks',
+            },
+            {
+                name: ' with Pork Chunks',
+                landRange: [a.city, a.village, a.tropical],
+            },
+            {
+                name: ' with Boar Chunks',
+                landRange: [a.forest, a.mountain],
+            },
+            {
+                name: ' with Chick Peas, Coriander',
+                needs: [a.desert],
+            },
+            {
+                name: ' with Lamb Chunks',
+                landRange: [a.desert, a.mountain],
+                misfits: [a.poor],
+            },
+            {
+                name: ' with Camel Chunks',
+                needs: [a.desert],
+                misfits: [a.poor],
+            },
+            {
+                name: ' with Beans, Onions',
+            },
+            {
+                name: ' with Dried Squid',
+                needs: [a.haven],
+                misfits: [a.poor],
+            },
+            {
+                name: ' with Pumpkin Chunks',
+                landRange: [a.forest, a.village],
+            },
+            {
+                name: ' with Shrimps',
+                needs: [a.haven],
+                incomeRange: [a.modest],
+            },
+            {
+                name: ' with Anchovies',
+                needs: [a.haven],
+                incomeRange: [a.poor],
+            },
+            {
+                name: ' with Smoked Salmon',
+                landRange: [a.haven, a.village, a.city, a.modest],
+            },
+            {
+                name: ' with Boar Feet',
+                landRange: [a.village, a.forest, a.mountain],
+                incomeRange: [a.poor],
+            },
+            {
+                name: ' with Pig Feet',
+                landRange: [a.village, a.city, a.forest],
+                incomeRange: [a.poor],
+            },
+            {
+                name: ' with Pickled Mole',
+                landRange: [a.underdark],
+                incomeRange: [a.modest],
+            },
+            {
+                name: ' with Lizard Chunks',
+                landRange: [a.underdark],
+                incomeRange: [a.wealthy],
+            },
+            {
+                name: ' with Minced Earth Worms',
+                landRange: [a.underdark],
+                incomeRange: [a.poor],
+            },
+        ],
+        secondSideDishes: [
+            {
+                name: ' and Bread Chunks',
+                landRange: [a.city, a.village],
+                needs: [a.poor],
+            },
+            {
+                name: ' and Potatoes',
+                incomeRange: [a.modest],
+            },
+            {
+                name: ' and Cabbage',
+                landRange: [a.forest],
+                incomeRange: [a.poor],
+            },
+            {
+                name: ' and Turmeric Rice',
+                needs: [a.desert],
+                incomeRange: [a.modest],
+            },
+            {
+                name: ' and Coconut Rice',
+                needs: [a.tropical],
+                incomeRange: [a.modest],
+            },
+            {
+                name: ' and Brown Rice',
+                needsOne: [a.tropical, a.desert, a.haven],
+                incomeRange: [a.modest],
+            },
+            {
+                name: ' and Flat Bread',
+                needsOne: [a.tropical, a.mountain, a.desert],
+                incomeRange: [a.poor],
+            },
+        ],
+    },
+    STEW_PRICE_FACTOR,
+    Eatable.mainDish
+);
